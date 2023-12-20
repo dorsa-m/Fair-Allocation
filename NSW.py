72f5dcb4898969ad70971731d47ad42c238af117
Dorsa Majdi <dorsa.majdi@gmail.com>
2023-12-20 09:11:05 +0100

    NSW edit

--- NSW.py
+++ NSW.py
@@ -23,11 +23,14 @@ def nash_social_welfare_maximization(values1, values2):
         bundle2 = set(range(m)) - bundle1
 
         welfare = sum(values1[i] for i in bundle1) * sum(values2[j] for j in bundle2)
+        print(bundle1, welfare)
+        print('*****')
 
         if welfare > max_welfare:
             max_welfare = welfare
             max_partition = (list(bundle1), list(bundle2))
 
+    print(max_welfare)
     return max_partition
 
 def create_linear_program(V, target):
@@ -43,42 +46,49 @@ def create_linear_program(V, target):
     target_prime = set(range(m)) - set(target)
     lhs = lpSum([a[i] for i in target]) * lpSum([V[j] for j in target_prime])
     lp_problem += lpSum(a) == 1
+    eps = 0.001
     for partition in all_partitions:
         if set(partition) == set(target):
             continue
         bundle = set(partition)
         bundle_prime = set(range(m)) - bundle
     # Constraints
-        rhs = lpSum([a[i] for i in bundle]) * lpSum([V[j] for j in bundle_prime])
+        rhs = lpSum([a[i] for i in bundle]) * lpSum([V[j] for j in bundle_prime]) + eps
         lp_problem += lhs >= rhs
 
     return lp_problem
 
 def check_feasibility(V, target):
     lp_problem = create_linear_program(V, target)
-    # print(lp_problem)
+    print(lp_problem)
+
     # Solve the linear program
     lp_problem.solve()
 
     # Check feasibility
     if LpStatus[lp_problem.status] == 'Optimal':
         print("Feasible")
+        lst = []
         for var in lp_problem.variables():
             if var.name.startswith('a_'):
                 print(f"{var.name} = {var.varValue}")
+                lst.append(var.varValue)
+        return lst
     else:
         print("Infeasible")
 
-def NSW_max_driver():
+def NSW_max_driver(values1, values2):
     # Example usage
-    values1 = [0.1, 0.2, 0.3, 0.4]
-    values2 = [0.1, 0, 0.4, 0.5]
+    # values1 = [0.1, 0.2, 0.3, 0.4]
+    # values2 = [0.1, 0, 0.4, 0.5]
 
     allocation = nash_social_welfare_maximization(values1, values2)
     print("Nash Social Welfare Maximizing Allocation:")
     print("Bundle 1:", allocation[0])
     print("Bundle 2:", allocation[1])
 
-check_feasibility([0.1,0.2,0.3,0.4], [1,3])
-# NSW_max_driver()
+
+lst = check_feasibility([0.1,0.2,0.3,0.4], [1,3])
+# check_feasibility([0.25 * element for element in [1,1,1,1]], [1,3,2])
+NSW_max_driver([0.1,0.2,0.3,0.4],lst)
 
